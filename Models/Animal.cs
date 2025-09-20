af6a6b0d2b86256a9fade39e2ec234692e216fba
isa2206 <hidalgoavilaisabel@gmail.com>
2025-09-19 21:04:48 -0400

    Primer commit

---
+++ Models/Animal.cs
@@ -0,0 +1,7 @@
+﻿public class Animal
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Species { get; set; } = "";
+    public int Age { get; set; }
+}
